a31817f5858294e4407fd08c034baad89742d05c
Anil Kumar <thomsonian2016@gmail.com>
2026-09-19 14:18:57 +0530

    Delete upi-calculator directory

--- upi-calculator/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [sitemap()]
-});
